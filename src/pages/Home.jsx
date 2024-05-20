562da54e428756ba3fe97a7af03d6d30bb9bba16
Joseph Victorio <136730548+Joseph-Victorio@users.noreply.github.com>
2024-05-20 17:08:22 +0700

    home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -70,7 +70,7 @@ const Home = () => {
               </div>
           </div>
         </section>
-        <ProdukSaya/>
+        {/* <ProdukSaya/> */}
         <TentangKamiSection/>
         <YangPercaya/>
         <ApaKataMereka/>
