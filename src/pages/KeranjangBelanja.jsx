dff52a522f4cd363f4063b35baf80f3785415d01
Joseph Victorio <136730548+Joseph-Victorio@users.noreply.github.com>
2024-07-22 13:52:18 +0700

    change color

--- src/pages/KeranjangBelanja.jsx
+++ src/pages/KeranjangBelanja.jsx
@@ -185,7 +185,7 @@ const KeranjangBelanja = () => {
                           <p className="text-primary text-[20px]">
                             {bar.value.nama_produk}
                           </p>
-                          <p className="text-secondary text-[18px]">
+                          <p className="text-primary text-[18px]">
                             {formatCurrencyIDR(bar.value.harga)}
                           </p>
                         </div>
@@ -218,7 +218,7 @@ const KeranjangBelanja = () => {
                         </div>
                       </div>
                     </div>
-                    <p className="text-secondary text-[10px] md:text-[15px]">
+                    <p className="text-primary text-[10px] md:text-[15px]">
                       {formatCurrencyIDR(bar.value.jumlah * bar.value.harga)}
                     </p>
                   </div>
@@ -329,13 +329,13 @@ const KeranjangBelanja = () => {
           <div className="p-5">
             <div className="mt-5">
               <p className="text-primary text-[18px] md:text-[24px]">
-                Total Produk ({localStorage.length}): <span className="text-secondary">{formatCurrencyIDR(totalSum)}</span>
+                Total Produk ({localStorage.length}): <span className="text-yellow-500">{formatCurrencyIDR(totalSum)}</span>
               </p>
               <p className="text-primary text-[18px] md:text-[24px]">
-                Waktu Sewa: <span className="text-secondary">{daysDifference} hari</span> 
+                Waktu Sewa: <span className="text-primary">{daysDifference} hari</span> 
               </p>
               <p className="text-primary text-[18px] md:text-[24px]">
-                Total Sementara: <span className="text-secondary">{formatCurrencyIDR(totalSum * daysDifference)}</span>
+                Total Sementara: <span className="text-primary">{formatCurrencyIDR(totalSum * daysDifference)}</span>
               </p>
             </div>
           </div>
